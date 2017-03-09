eda9ed2fe91977ee92b52823c526cfd547571b33
Justin Jamison <justin.jamison@outlook.com>
2017-03-08 18:41:14 -0700

    MongoDB stuff.

---
+++ controllers/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const dbPromise = require('../database')('test');
+
+// exports.addTask = function(title, description, date) {
+// 	const o = {
+// 		title: title,
+// 		description: description,
+// 		dueDate
+// 	};
+
+// 	return dbPromise.then
+// };
---
+++ server/app.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+
+module.exports = function(dbName) {
+	// Connection URL
+	const url = 'mongodb://localhost:27017/' + dbName;
+	return MongoClient.connect(url);
+
+	// Use connect method to connect to the server
+	// MongoClient.connect(url)
+	// 	.then(function(err, db) {
+	// 		if (err) console.error(err.stack);
+	//   		console.log("Connected successfully to server");
+
+	//   		db.close();
+	// });
+}
+
