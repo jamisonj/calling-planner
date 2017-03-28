c3ecbae2d4276af37f5ba88f3217a5ab88f9ab93
Justin Jamison <Justin Jamison>
2017-03-27 23:15:07 -0600

    More work on login/logout. Trying to get logout working properly.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const passport            = require('passport');
 const session             = require('express-session');
 
 const app = express();
+var data = {};
 
 // tell passport to use a local strategy and tell it how to validate a username and password
 passport.use(new LocalStrategy(function(username, password, done) {
@@ -18,12 +19,12 @@ passport.use(new LocalStrategy(function(username, password, done) {
 
 // tell passport how to turn a user into serialized data that will be stored with the session
 passport.serializeUser(function(user, done) {
-    done(null, user.username);
+    done(null, user);
 });
 
 // tell passport how to go from the serialized data back to the user
-passport.deserializeUser(function(id, done) {
-    done(null, { username: id });
+passport.deserializeUser(function(user, done) {
+    done(null, { username: user.username });
 });
 
 // tell the express app what middleware to use
@@ -46,24 +47,40 @@ app.get('/api/protected',
     }
 );
 
+// app.get('/brainstorm', function(req, res) {
+//     if (!req.user) return res.sendStatus(401);
+//     res.sendFile('/brainstorm', {root: './www/src'});
+// });
+
+// app.get('/staging', function(req, res) {
+//     if (!req.user) return res.sendStatus(401);
+//     res.sendFile('/staging', {root: './www/src'});
+// });
+
+// app.get('/final', function(req, res) {
+//     if (!req.user) return res.sendStatus(401);
+//     res.sendFile('/final', {root: './www/src'});
+// });
+
 // specify the login url
-app.put('/api/auth',
-    passport.authenticate('local'),
-    function(req, res) {
-        console.log(req.user);
-        res.send(req.user.username);
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+
+    // If the user doesn't have an entry in data, add one.
+    if (!data[req.user.username]) {
+        data[req.user.username] = req.user;
+    }
+
+    res.send(data[req.user.username]);
 });
 
 // log the user out
-app.delete('/api/auth', function(req, res) {
+app.delete('/api/logout', function(req, res) {
     req.logout();
     res.send('You have logged out.');
     res.redirect('/');
 });
 
 app.get('*', function(req, res) {
-	if (req.user) return res.send('Hello, ' + req.user.username);
-    res.send('Hello, Stranger!');
 	console.log(req.path);
 	// let fullPath = path.resolve(__dirname, 'www/index.html');
 	// console.log(fullPath);
