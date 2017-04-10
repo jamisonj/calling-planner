dd1cd6bad1b708b626defbbbbd5884f06079d027
Sean Bird <bird.seanmichael@gmail.com>
2017-04-10 08:25:41 -0600

    got login hooked up to database

--- server.js
+++ server.js
@@ -7,24 +7,42 @@ const express             = require('express');
 const LocalStrategy       = require('passport-local').Strategy;
 const passport            = require('passport');
 const session             = require('express-session');
+const mongoose            = require('mongoose');
 
 const app = express();
 var data = {};
 
+// login --------------------------------------------------------------------------------
+
 // tell passport to use a local strategy and tell it how to validate a username and password
 passport.use(new LocalStrategy(function(username, password, done) {
-    if (username && password) return done(null, { username: username });
-    return done(null, false);
+  process.nextTick(function() {
+    UserDetails.findOne({
+      'username': username, 
+    }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      if (user.password != password) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  });
 }));
 
-// tell passport how to turn a user into serialized data that will be stored with the session
 passport.serializeUser(function(user, done) {
-    done(null, user);
+  done(null, user);
 });
 
-// tell passport how to go from the serialized data back to the user
 passport.deserializeUser(function(user, done) {
-    done(null, { username: user.username });
+  done(null, user);
 });
 
 // tell the express app what middleware to use
@@ -34,6 +52,21 @@ app.use(session({ secret: 'secret key', resave: false, saveUninitialized: true }
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static('www'));
+
+//mongoose user model ---------------------------------------------------------------
+
+mongoose.connect('mongodb://localhost/MyDatabase');
+
+var Schema = mongoose.Schema;
+var UserDetail = new Schema({
+      username: String,
+      password: String
+    }, {
+      collection: 'userInfo'
+    });
+var UserDetails = mongoose.model('userInfo', UserDetail);
+
+//routes -------------------------------------------------------------------------
 
 app.use('/api', bodyParser.json());
 // app.use('/api/tasks', taskRoute);
@@ -80,15 +113,12 @@ app.post('/api/register', passport.authenticate('local'), function(req, res) {
 });
 
 // Login URL.
-app.post('/api/login', passport.authenticate('local'), function(req, res) {
-
-    // If the user doesn't have an entry in data, add one.
-    if (!data[req.user.username]) {
-        console.log(req.user);
-        data[req.user.username] = req.user;
-    }
-
-    res.send(data[req.user.username]);
+app.post('/api/login', function(req, res, next ){
+    passport.authenticate('local', function(err, user, info) {
+      if (err) { return next(err) }
+      if (!user) { return res.json( { message: "Incorrect credentials" }) }
+      res.json(user);
+    })(req, res, next);   
 });
 
 // Logout URL.
